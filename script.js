dd56dd7cd17e13cfe80d5123ede2b88f15ff9a18
Xander Zzen <epin.92@mail.ru>
2025-02-17 23:50:19 +0300

    second_commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const apiKey = 'fdf7f0f8e946c54b36d201ff70f19477';
-btn = document.getElementById('btn');
+const apiKey = "fdf7f0f8e946c54b36d201ff70f19477";
+btn = document.getElementById("btn");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   event.preventDefault();
-  const inputText = document.getElementById('input-text');
+  const inputText = document.getElementById("input-text");
   const cityName = inputText.value;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&q=${cityName}&appid=${apiKey}`;
   checkWeather(apiUrl);
@@ -13,11 +13,13 @@ btn.addEventListener('click', () => {
 async function checkWeather(apiUrl) {
   const response = await fetch(apiUrl);
   const data = await response.json();
-  console.log(data, 'data');
+  console.log(data, "data");
 
-  document.querySelector('.name').innerHTML = data.name;
-  document.querySelector('.temp').innerHTML =
-    'температура воздуха: ' + data.main.temp;
-  document.querySelector('.temp-feel').innerHTML =
-    'ощущается как: ' + data.main.feels_like;
+  document.querySelector(".name").innerHTML = data.name;
+  document.getElementsByClassName("weather")[0].innerHTML =
+    "погода: " + data.weather[0].description;
+  document.querySelector(".temp").innerHTML =
+    "температура воздуха: " + Math.round(data.main.temp);
+  document.querySelector(".temp-feel").innerHTML =
+    "ощущается как: " + Math.round(data.main.feels_like);
 }
