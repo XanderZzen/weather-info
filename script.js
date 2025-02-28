42dbf8fa6befd611c08089197801535a3c0b10f7
Xander Zzen <epin.92@mail.ru>
2025-02-28 19:28:22 +0300

    commit-6

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const apiKey = "fdf7f0f8e946c54b36d201ff70f19477";
-btn = document.getElementById("btn");
+const apiKey = 'fdf7f0f8e946c54b36d201ff70f19477';
+btn = document.getElementById('btn');
 
-btn.addEventListener("click", () => {
+btn.addEventListener('click', () => {
   event.preventDefault();
-  const inputText = document.getElementById("input-text");
+  const inputText = document.getElementById('input-text');
   const cityName = inputText.value;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&q=${cityName}&appid=${apiKey}`;
   checkWeather(apiUrl);
@@ -13,31 +13,28 @@ btn.addEventListener("click", () => {
 async function checkWeather(apiUrl) {
   const response = await fetch(apiUrl);
   const data = await response.json();
-  console.log(data, "data");
+  console.log(data, 'data');
   let weather;
-  document.querySelector(".name").innerHTML = data.name;
-  document.getElementsByClassName("weather")[0].innerHTML =
-    "погода: " + data.weather[0].description;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector('.name').innerHTML = data.name;
+  document.getElementsByClassName('weather')[0].innerHTML =
+    data.weather[0].description;
+  document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
 
   switch (data.weather[0].main) {
-    case "Clouds":
+    case 'Clouds':
       weather =
-        '<img src="./weather_icons/icons8-cloud-lightning-100.png" alt="" />';
+        '<img class="condit-img" src="./weather_icons/clouds.png" alt="" />';
+      break;
+    case 'Clear':
+      weather =
+        '<img class="condit-img" src="./weather_icons/clear.png" alt="" />';
+      break;
+    case 'Rain':
+      weather =
+        '<img class="condit-img" src="./weather_icons/rain.png" alt="" />';
       break;
     default:
-      weather = "lol";
+      weather = 'lol';
   }
-  document.getElementById("weather-img").innerHTML = weather;
+  document.getElementById('weather-img').innerHTML = weather;
 }
-
-// Weather condition:
-// clear sky
-// few clouds
-// scattered clouds
-// broken clouds
-// shower rain
-// rain
-// thunderstorm
-// snow
-// mist
